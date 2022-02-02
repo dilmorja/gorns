ba81189a557b4e8edb76a9baa0850eb157515026
pszao <pszao.dev@gmail.com>
2022-02-02 03:08:29 -0500

    [Add] package overview

---
+++ doc.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2022, Daniel M. Jaén <pszao.dev@gmail.com>
+// All rights reserved.
+
+/*
+This package provides what you need to work with warnings.
+
+The purpose of warnings is to prevent a runtime error from interfering with the execution of the program,
+but still processing it.
+Consequently, the developer is forced to avoid everything depending on one thing.
+*/
+package gorns
