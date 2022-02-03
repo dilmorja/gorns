4e09a08f246c5f62a08e6edddcadddadc3ea9a0e
pszao <pszao.dev@gmail.com>
2022-02-03 16:02:15 -0500

    [Delete, Update] `Uwrap`, `Wrap` to `Warnf`

--- gorns.go
+++ gorns.go
@@ -22,22 +22,15 @@ type UWarn struct {
 // This should be used to contain warning information.
 type Warn interface {
 	// Warn type as string
-	Wrap() string
-	// Wrapped UWarn type to Unwrapped UWarn type
-	Unwrap() *UWarn
+	Warnf() string
 }
 
 // Formats an element of type UWarn and returns a string.
 // Its use is perfect for terminals, web applications, etc.
-func (uw *UWarn) Wrap(format string, v ...interface{}) string {
-	if len(v) <= 2 {
+func (uw *UWarn) Warnf(format string, v ...interface{}) string {
+	if len(v) > 0 {
 		return fmt.Sprintf(format, v...)
 	}
 
 	return fmt.Sprintf("%s (%d): %s", uw.Name, uw.Code, uw.Content)
-}
-
-// Returns an element of type UWarn that was previously wrapped
-func (uw *UWarn) Unwrap() *UWarn {
-	return uw
 }
