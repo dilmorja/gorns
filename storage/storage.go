6eb70ed3ac2d238e16d1792051b1fb4e65ca64c1
pszao <pszao.dev@gmail.com>
2022-02-05 03:48:31 -0500

    [Add] storage This package is used to manage the storage of runtime warnings. For more information: please follow the go reference or the code comments.

---
+++ storage/storage.go
@@ -0,0 +1,85 @@
+package storage
+
+import (
+	"sync"
+
+	"github.com/pszao/gorns"
+)
+
+// This structure is private to prevent inappropriate or unwanted use.
+type storage struct {
+	warns map[string]*gorns.UWarn
+	mutex sync.RWMutex
+	limit int16
+}
+
+// The storage structure is like the cache, only its elements do not expire.
+// Instead, you can and should set a space limit for storing new items.
+type Storage struct {
+	*storage
+}
+
+// The configuration structure required to create a new storage instance.
+type StorageConfig struct {
+	// This limit must be carefully set.
+	// If incorrectly assigned,
+	// it could result in an unexpected crash of the application,
+	// or otherwise uninterrupted execution (unless there is some fatal runtime error).
+	Limit int16
+}
+
+// Create a new storage instance.
+//		storage := gorns.NewStorage(&StorageConfig{
+//			Limit: int16(6), // Converting from int to int16 is recommended.
+//		})
+// If the storage variable is incorrectly modified,
+// there is a risk of losing the warnings stored so far.
+func NewStorage(sc *StorageConfig) Storage {
+
+	this := new(storage)
+	this.limit = sc.Limit
+	this.warns = make(map[string]*gorns.UWarn)
+	this.mutex = sync.RWMutex{}
+
+	return Storage{
+		this,
+	}
+
+}
+
+// Save a new warning if it doesn't exist;
+// otherwise it returns the existing warning.
+// If the warning does not exist and the storage limit has not been reached;
+// continue with the build process; otherwise return nil.
+//		// ...
+//		warn := storage.Push(&UWarn{
+//			Name: "EXAMPLE",
+//			Code: uint16(69),
+//			Content: "Hi! This is a example. :D",
+//		})
+//		// ...
+func (s *storage) Push(warn *gorns.UWarn) *gorns.UWarn {
+	if w := s.Get(warn.Name); w != nil {
+		return w
+	}
+	if int16(len(s.warns)) != s.limit {
+		s.mutex.Lock()
+		s.warns[warn.Name] = warn
+		s.mutex.Unlock()
+	}
+	return nil
+}
+
+// Get the object of a warning if it exists; otherwise it returns nil.
+//		// ...
+//		getWarn := storage.Get("EXAMPLE")
+//		// ...
+func (s *storage) Get(name string) *gorns.UWarn {
+	s.mutex.RLock()
+	if w, ok := s.warns[name]; ok {
+		s.mutex.RUnlock()
+		return w
+	}
+	s.mutex.RUnlock()
+	return nil
+}
---
+++ storage/storage_test.go
@@ -0,0 +1,94 @@
+package storage
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/pszao/gorns"
+)
+
+func Test_Storage(t *testing.T) {
+	expected := "*storage.Storage"
+
+	st := new(Storage)
+
+	got := reflect.TypeOf(st).String()
+
+	if expected != got {
+		t.Errorf("Expected: %s, got: %s", expected, got)
+	}
+}
+
+func Test_Push(t *testing.T) {
+	expected := "TEST"
+
+	se := NewStorage(&StorageConfig{
+		Limit: int16(6),
+	})
+
+	se.Push(&gorns.UWarn{
+		Name:    "TEST",
+		Code:    uint16(84),
+		Content: "test",
+	})
+
+	got := se.Get("TEST").Name
+
+	if expected != got {
+		t.Errorf("Expected: %s, got: %s", expected, got)
+	}
+}
+
+func Test_Push_Same(t *testing.T) {
+	expected := "TEST"
+	var got string
+
+	se := NewStorage(&StorageConfig{
+		Limit: int16(6),
+	})
+
+	_ = se.Push(&gorns.UWarn{
+		Name:    "TEST",
+		Code:    uint16(84),
+		Content: "test",
+	})
+
+	warn := se.Push(&gorns.UWarn{
+		Name:    "TEST",
+		Code:    uint16(84),
+		Content: "test",
+	})
+
+	if warn != nil {
+		got = warn.Name
+	} else {
+		got = se.Get("TEST").Name
+	}
+
+	if expected != got {
+		t.Errorf("Expected: %s, got: %s", expected, got)
+	}
+}
+
+func Test_PushLimit(t *testing.T) {
+
+	se := NewStorage(&StorageConfig{
+		Limit: int16(1),
+	})
+
+	se.Push(&gorns.UWarn{
+		Name:    "TEST",
+		Code:    uint16(84),
+		Content: "test",
+	})
+
+	got := se.Push(&gorns.UWarn{
+		Name:    "TEST_TWO",
+		Code:    uint16(168),
+		Content: "test",
+	})
+
+	if got != nil {
+		t.Errorf("Expected: %v, got: %v", nil, got)
+	}
+}
